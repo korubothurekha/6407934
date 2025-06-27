a55ee3ca348329e6b54aa5dc8afb45925ea45f1d
korubothurekha <147308797+korubothurekha@users.noreply.github.com>
2025-06-27 10:47:39 +0530

    ..

--- Exercise_5/LogTransaction.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TRIGGER LogTransaction
-AFTER INSERT ON Transactions
-FOR EACH ROW
-BEGIN
-    INSERT INTO AuditLog(transaction_id, action, log_time)
-    VALUES (:NEW.transaction_id, 'INSERT', SYSDATE);
-END;
