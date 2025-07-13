4a168628224a0f589e828bbfcd9dc9e256bacbcd
korubothurekha <147308797+korubothurekha@users.noreply.github.com>
2025-07-14 02:05:33 +0530

    ..

--- Week4_Spring/Week3_Spring/Exercise-4/code/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public String getBookData() {
-        return "Data fetched from Book Repository";
-    }
-}
--- Week4_Spring/Week3_Spring/Exercise-5/code/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void getBookDetails() {
-        System.out.println("BookRepository: Fetching book details...");
-    }
-}
